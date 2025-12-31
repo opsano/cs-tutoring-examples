1130ce28873d279b6b0518627774e07d91ccd2bf
opsano <greennates@gmail.com>
2025-12-31 07:00:35 -0800

    implemented the ability to hash data, pushing so I can work on it on my laptop later, this is by no means done

--- Java/hashTable/HashTable.java
+++ Java/hashTable/HashTable.java
@@ -12,8 +12,25 @@ public class HashTable<K,V> { // takes a generic key, value pair
         return false; // placeholder
     }
 
-    private int hash(K key){
-        return 0; // placeholder
+    // returns position in array to place data
+    public int hash(K key){
+        int position = key.hashCode();
+        /*
+        the 0x7fffffff bit just forces the hash to be positive
+        in memory, we have 1 sign bit then 31 value bits, where a 1 for a sign bit is negative and a 0 is positive
+
+        ex our number is -4:
+        10000000 00000000 00000000 00000100 <- negative 4
+        01111111 11111111 11111111 11111111 <- binary representation of 0x7fffffff
+        bitwise & operator:
+        00000000 00000000 00000000 00000100
+
+        the bitwise and operator goes bit by bit and compares two numbers.
+        for bitwise and, 1 & 1 = 1, 1 & 0 = 0, 0 & 1 = 0
+        so 0x7fffffff changes case, but keeps value bits
+        this needs to be positive, because we can't have a negative array position
+         */
+        return (position & 0x7fffffff) % sizeOfTable;
     }
 
     /* TODO:
--- Java/hashTable/Main.java
+++ Java/hashTable/Main.java
@@ -2,6 +2,7 @@ package hashTable;
 
 public class Main {
     public static void main(String[] args){
-
+        HashTable<String, String> tble = new HashTable<>();
+        System.out.println(tble.hash("names"));
     }
 }
