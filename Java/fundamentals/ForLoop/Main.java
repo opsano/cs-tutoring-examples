8fcc01ff89f7d62083a4c32b57bfa9c971fe788a
opsano <greennates@gmail.com>
2026-02-13 00:17:20 -0800

    Updated some comments for the for loop tutorial

--- Java/fundamentals/ForLoop/Main.java
+++ Java/fundamentals/ForLoop/Main.java
@@ -3,20 +3,20 @@ package fundamentals.ForLoop;
 public class Main {
     public static void main(String[] Args){
         /*
-        * The for loop allows us to combine our variable initialization, condition, and
+        * The "for loop" allows us to combine our variable initialization, condition, and
         * increment/decrement in a single statement.
-        * Anything done with a for loop can also be done with a while loop! It just might be easier
-        * to think about with a for loop
+        * Anything done with a "for loop" can also be done with a "while loop"! It just might be easier
+        * to think about with a "for loop"
         *
-        * To use a for loop, you use the "for" keyword. Then within parenthesis put a
-        * variable that acts as your counter, a condition which MUST be answered by true or false,
+        * To use a "for loop", you use the "for" keyword. Then within parenthesis put a
+        * variable that acts as your counter, a condition which MUST result in either true or false,
         * and an increment/decrement to the counter variable.
         *
-        * Each of those statements is separated by a semicolon, and the part that changes inside the
-        * loop is within curly brackets {}
+        * Each of those statements is separated by a semicolon, and the work that you do within the
+        * loop is surrounded by curly brackets {}
         *
         * By convention, we call our temporary variable i. After the loop ends, the variable gets
-        * removed from our memory and can be re-initialized in another for loop.
+        * removed from our memory and can be re-initialized in another "for loop".
         *
         * If we were to create a loop inside another loop (this is called nesting), by convention
         * we use j, then k, and you should hopefully never need to nest more than 3 loops.
