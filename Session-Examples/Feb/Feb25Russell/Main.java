b5f2bdb74f41353cbcf74f60f74a80dcd2afcbaf
opsano <greennates@gmail.com>
2026-02-25 13:03:06 -0800

    2/25/26 session - Russell

---
+++ Session-Examples/Feb/Feb25Russell/Main.java
@@ -0,0 +1,16 @@
+package Feb.Feb25Russell;
+
+public class Main {
+    public static void main(String[] Args){
+        // inputs
+        int variable1 = 5;
+        int variable2 = 3;
+
+        // process
+        int product = variable1 * variable2;
+        int directProduct = 5 * 3;
+        // output
+        System.out.println(product); // prints 15
+        System.out.println(directProduct); // prints 15 as well
+    }
+}
