dbb4047bcde342db77a51e344f701e8de62743f2
opsano <greennates@gmail.com>
2026-02-26 13:13:23 -0800

    2/26/26 session - Russell

---
+++ Session-Examples/Feb/Feb26Russell/Main.java
@@ -0,0 +1,34 @@
+package Feb.Feb26Russell;
+
+public class Main {
+    public static void main(String[] args){
+        // initialize variables
+        // type name = value;
+        int height = 600;
+
+        // common convention for naming variables
+        int the_dog_variable = 5;
+        int theDogVariable = 5; // called camel case
+
+        // difference between float and double
+        float myFloat = 55.5f; // REQUIRES the f
+        double myDouble = 55.5;
+
+        // printing out variables
+        System.out.println(height);
+        System.out.println(height);
+        /*
+        * prints:
+        * 600
+        * 600
+        */
+        // difference between println & print
+        System.out.print(height);
+        System.out.print(height);
+        /*
+        * prints:
+        * 600600
+        */
+
+    }
+}
