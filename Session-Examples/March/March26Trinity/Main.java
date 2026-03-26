7039bbbb41fec2e86ac79e8f7b52519f6c8a4e7b
opsano <greennates@gmail.com>
2026-03-26 12:43:14 -0700

    3/26/26 session - Trinity

---
+++ Session-Examples/March/March26Trinity/Main.java
@@ -0,0 +1,37 @@
+package March.March26Trinity;
+
+public class Main {
+    public static void main(String[] Args){
+        Employee Joe = new Employee();
+        Employee John = new Employee();
+        Joe.setWage(5);
+        John.setWage(10);
+        System.out.println(John.getWage());
+        System.out.println(Employee.getNumberOfEmployees());
+    }
+}
+
+class Employee{
+    // belongs to the class itself rather than any individual instances of it, will persist across any individual object
+    private static int numberOfEmployees;
+    private String name; // belongs to objects, the class itself can't see this, and objects can't see the static variable.
+    private int wage;
+    Employee(){
+        numberOfEmployees++; // every time we make an employee, we want to increase the total number of employees
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getWage(){
+        return wage;
+    }
+    public void setWage(int wage){
+        this.wage = wage;
+    }
+
+    // this is a static method, meaning you shouldn't use a specific object to call it, instead use the Employee class itself.
+    public static int getNumberOfEmployees(){
+        return numberOfEmployees;
+    }
+}
