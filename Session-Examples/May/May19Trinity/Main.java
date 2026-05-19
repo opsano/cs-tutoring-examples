0854f7d9c7ccdcf1d70078caa9f10111b4567ea4
opsano <greennates@gmail.com>
2026-05-19 14:45:40 -0700

    5/19/26 session -trinity

--- Session-Examples/May/May19Trinity/Main.java
+++ Session-Examples/May/May19Trinity/Main.java
@@ -3,6 +3,12 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] Args){
+        /*
+        * get input for an integer
+        * loop while the input from the user is valid
+        * if it's not valid, express that input was wrong, and loop again
+        */
+
         Scanner kbd = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int myInt;
@@ -18,7 +24,6 @@ public class Main {
                 inputValid = false;
             }
         } while(!inputValid);
-
         String[] myArray = new String[3];
         //OR//
         String[] myOtherArray = {"hello", "world", "!"};
@@ -27,5 +32,7 @@ public class Main {
         for(String myString : myOtherArray){
             System.out.println(myString);
         }
+
+
     }
 }
